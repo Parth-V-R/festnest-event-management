b2ea8dd929662b483e1f277f2d5bfa8ea1150911
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-03-19 23:10:34 +0530

    updated crucial folders

--- events/models.py
+++ events/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    CATEGORY_CHOICES = [
+        ('cultural', 'Cultural'),
+        ('technical', 'Technical'),
+        ('sports', 'Sports'),
+        ('nss', 'NSS'),
+    ]
+
+    title = models.CharField(max_length=200)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    date = models.DateField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
--- events/views.py
+++ events/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .models import Event
 
 def home(request):
-    return render(request, 'index.html')
+    events = Event.objects.all()
+    return render(request, 'index.html', {'events': events})
+
+def category_events(request, category):
+    events = Event.objects.filter(category=category)
+    return render(request, 'category.html', {
+        'events': events,
+        'category': category
+    })
